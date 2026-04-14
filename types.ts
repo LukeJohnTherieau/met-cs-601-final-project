f624975e475c32928b0e5a263d7325a87f684071
Luke Therieau <luketherieau@outlook.com>
2026-04-14 17:35:33 -0400

    Update types.ts

---
+++ types.ts
@@ -0,0 +1,22 @@
+export type JobApplicationProps = {
+    _id?: string;
+    title: string;
+    dateApplied: Date;
+    status: string;
+    applicationURL: string;
+    positionType: string;
+    startingPay: number;
+    endingPay: number;
+    company: {
+        name: string;
+        industries: string[];
+        location: string;
+        companyURL: string;
+    };
+};
+
+
+export type SubmissionStatusProps = {
+    successfulSubmission: boolean;
+    message: string;
+};
